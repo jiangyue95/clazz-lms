91818730ac896d4191da75752c174347b1e00512
jiangyue95 <yuejiang811@gmail.com>
2026-07-08 23:35:30 +0100

    feat: implement S3 file storage with presigned URL access

--- clazz-lms-server/src/main/java/com/yue/controller/UploadController.java
+++ clazz-lms-server/src/main/java/com/yue/controller/UploadController.java
@@ -1,16 +1,13 @@
 package com.yue.controller;
 
 import com.yue.pojo.Result;
-import com.yue.utils.AliyunOSSOperator;
+import com.yue.utils.FileStorage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 /**
  * File upload REST controller.
@@ -29,29 +26,11 @@ import java.io.IOException;
 @RestController
 public class UploadController {
 
-    /**
-     * 本地存储文件
-     * @param name
-     * @param age
-     * @param file
-     * @return
-     * @throws IOException
-     */
-//    @PostMapping("/upload")
-//    public Result upload(String name, Integer age, MultipartFile file) throws IOException {
-//        log.info("接收到的参数：{}, {}, {}", name, age, file);
-//        // 获取原始文件名
-//        String oringinalFilename =  file.getOriginalFilename();
-//
-//        // 新的文件名
-//        String extension = oringinalFilename.substring(oringinalFilename.lastIndexOf("."));
-//        String newFileName = UUID.randomUUID().toString() + extension;
-//        // 保存文件
-//        file.transferTo(new File("/Users/jiangyue/temp/" + newFileName));
-//        return Result.success();
-//    }
-    @Autowired
-    private AliyunOSSOperator aliyunOSSOperator;
+    private final FileStorage fileStorage;
+
+    public UploadController(FileStorage fileStorage) {
+        this.fileStorage = fileStorage;
+    }
 
     @Operation(
             summary = "Upload a file to cloud storage",
@@ -67,10 +46,10 @@ public class UploadController {
     )
     @PostMapping("/upload")
     public Result upload(MultipartFile file) throws Exception {
-        log.info("文件上传：{}", file);
-        // 将文件交给 OSS 存储管理
-        String url = aliyunOSSOperator.upload(file.getBytes(), file.getOriginalFilename());
-        log.info("文件上传OSS，url：{}", url);
-        return Result.success(url);
+        log.info("File upload：{}", file);
+
+        String key = fileStorage.upload(file.getBytes(), file.getOriginalFilename(), file.getContentType());
+        log.info("File upload to AWS S3, key={}", key);
+        return Result.success(key);
     }
 }
