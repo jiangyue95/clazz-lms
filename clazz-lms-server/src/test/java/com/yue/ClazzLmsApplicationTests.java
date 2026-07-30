d819066a6e1826e25851b5be224f0ed2efcc88f3
jiangyue95 <yuejiang811@gmail.com>
2026-07-30 23:43:32 +0100

    fix(test): clean up the tutorial-era test classes

--- clazz-lms-server/src/test/java/com/yue/ClazzLmsApplicationTests.java
+++ clazz-lms-server/src/test/java/com/yue/ClazzLmsApplicationTests.java
@@ -2,9 +2,11 @@ package com.yue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class TliasWebManagementApplicationTests {
+@ActiveProfiles("test")
+class ClazzLmsApplicationTests {
 
     @Test
     void contextLoads() {
